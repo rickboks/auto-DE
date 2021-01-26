5b2d4cdf1de229b3a432f20aa416d578ba178356
Rick Boks <rickboks@hotmail.com>
2021-01-26 15:19:21 +0100

    improved exponential crossover

--- src/crossovermanager.cc
+++ src/crossovermanager.cc
@@ -1,5 +1,7 @@
 #include "util.h"
 #include "crossovermanager.h"
+#include <algorithm>
+#include <functional>
 
 std::function<CrossoverManager* ()> CrossoverManager::create(std::string const id){
 #define LC(X) [](){return new X();}
@@ -21,17 +23,21 @@ Solution* BinomialCrossoverManager::crossover(Solution const* const target, Solu
 
 Solution* ExponentialCrossoverManager::crossover(Solution const* const target, Solution const* const donor, double const Cr) const{
 	VectorXd x = target->getX();
-	unsigned int const start = rng.randInt(0,x.size()-1);
+	int const start = rng.randInt(0,x.size()-1);
 
-	unsigned int L = 1;
-	while (rng.randDouble(0,1) <= Cr && L <= x.size())
+	int L = 1;
+	while (L < x.size() && rng.randDouble(0,1) <= Cr)
 		L++;
 
-	unsigned int const end = (start+L-1) % x.size();
-	for (unsigned int i = 0; i < x.size(); i++){
-		if ((end >= start && (i >= start && i <= end)) || (start > end && (i <= end || i >= start)))
+	int const end = (start+L-1) % x.size();
+	auto const condition = end >= start ? \
+		[](int const i, int const start, int const end) {return i >= start && i <= end;}: 
+		[](int const i, int const start, int const end) {return i <= end || i >= start;};
+
+	for (unsigned int i = 0; i < x.size(); i++)
+		if (condition(i, start, end))
 			x[i] = donor->getX(i);
-	}
+
 	return new Solution(x);
 }
 
