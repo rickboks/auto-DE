24e474f2d0decc2a5f52bbb9a7eb0bfc3f1fc86d
Rick Boks <rick@rickboks.xyz>
2021-02-02 10:09:35 +0100

    improved experiment.fish

--- experiment.fish
+++ experiment.fish
@@ -1,5 +1,6 @@
 #!/usr/bin/env fish
-set hostfile "hostfile"
+set executable "$HOME/auto-DE/experiment"
+
 set credit "DR" "SD" "FI"
 set mutation "BE2,RA2,TB1,TP1"
 set crossover "B"
@@ -7,9 +8,7 @@ set dimensions "20"
 set instances "1-24"
 set threads "32"
 
-set executable "$HOME/auto-DE/experiment"
 set hosts (preserve -llist | grep "$USER" | cut -f9- | tr ' ' ',')
-
 set configs (printf '%s\n' $mutation" "$crossover" "$credit | awk '{printf("%s %s_%s_%s\n",$0,$1,$2,$3);}')
-parallel --workdir "$HOME/auto-DE" -u -S $hosts -j $threads --colsep ' '\
+parallel --workdir (dirname $executable) -u -S $hosts -j $threads --colsep ' '\
 	$executable -m {1} -c {2} -C {3} -I {4} ::: $configs
