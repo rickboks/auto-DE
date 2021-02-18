0ddb7e4a8527b9add5ecebfb55fa82a1697c8243
Rick Boks <rickboks@hotmail.com>
2021-02-18 12:13:44 +0100

    tuning experiment

---
+++ src/tuning.cc
@@ -0,0 +1,126 @@
+#include <iostream>
+#include <getopt.h>
+#include "coco.h"
+#include "differentialevolution.h"
+#include "params.h"
+
+static coco_problem_t *PROBLEM;
+static int const BUDGET_MULTIPLIER = 1e4;
+
+void experiment(DifferentialEvolution& de,
+				char const *const suite_name,
+				char const *const suite_options,
+				char const *const observer_name,
+				char const *const observer_options) {
+
+	coco_suite_t *suite;
+	coco_observer_t *observer;
+	suite = coco_suite(suite_name, "instances: 1" , suite_options);
+	observer = coco_observer(observer_name, observer_options);
+
+	while ((PROBLEM = coco_suite_get_next_problem(suite, observer))) {
+		int const dimension = coco_problem_get_dimension(PROBLEM);
+		int const popSize = dimension * params::popsize_multiplier;
+		size_t const budget = dimension * BUDGET_MULTIPLIER;
+		std::string const fid = coco_problem_get_id(PROBLEM);
+
+		do {
+			de.run(PROBLEM, budget, popSize);
+		} while (!coco_problem_final_target_hit(PROBLEM) && coco_problem_get_evaluations(PROBLEM) < budget);
+	}
+
+	coco_observer_free(observer);
+	coco_suite_free(suite);
+}
+
+std::vector<std::string> splitString(std::string str){
+	std::vector<std::string> v; 
+	size_t pos = 0;
+	while ((pos = str.find(',')) != std::string::npos) {
+		std::string const token = str.substr(0, pos);
+		v.push_back(token);
+		str.erase(0, pos + 1);
+	}
+	v.push_back(str);
+	return v;
+}
+
+int main(int argc, char** argv) {
+	//coco_set_log_level("warning");
+	//
+	std::string config_id = argv[1];
+	std::string instance_id = argv[2];
+	std::string seed = argv[3];
+	argc-=3;
+	argv+=3;
+
+	// defaults
+	std::string 
+		strategy    = "A",
+		param		= "S",		
+		credit		= "SD",
+		reward 		= "AA",
+		quality 	= "WS",
+		probability	= "PM",
+		constraint 	= "RS",
+		//////////////////////
+		suite 		= "bbob",
+	  	dimensions 	= "20",
+	 	functions 	= "",
+		instances 	= "1";
+
+	std::string id = "DE";
+
+	std::vector<std::string>
+		mutation = {"BE1", "RA1", "TB2"},
+		crossover = {"B", "E"};
+
+	int c;
+
+#define OPT(X,Y) case X: Y = optarg; break;
+	while ((c = getopt(argc, argv, "P:C:r:q:p:b:d:f:i:I:m:c:s:A:B:")) != -1){
+		switch (c){
+			OPT('s', strategy)
+			OPT('P', param)
+			OPT('C', credit)
+			OPT('r', reward)
+			OPT('q', quality)
+			OPT('p', probability)
+			OPT('b', constraint)
+			OPT('d', dimensions)
+			OPT('f', functions)
+			OPT('I', id)
+			case 'A': params::WS_alpha = std::stod(optarg); break;
+			case 'B': params::AP_beta = std::stod(optarg); break;
+			case 'm': mutation = splitString(optarg); break;
+			case 'c': crossover = splitString(optarg); break;
+		}
+	}
+
+	DifferentialEvolution de(
+		id, 
+		{
+			.strategy = strategy,
+			.constraintHandler = constraint,
+			.strategyAdaptationConfig = { 
+				.mutation = mutation,
+				.crossover = crossover,
+				.param = param,
+				.credit = credit,
+				.reward = reward,
+				.quality = quality,
+				.probability = probability
+			}
+		}
+	);
+
+	experiment(
+		de, 
+		suite.c_str(), 
+		("dimensions: " + dimensions + " instance_indices: " + instances + " function_indices: " + functions).c_str(), 
+		suite.c_str(), 
+		("result_folder: " + id).c_str()
+	);
+
+	return 0;
+}
