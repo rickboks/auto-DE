50742b376a6afebfa00eac86d06f31027103d2ea
Rick Boks <rick@rickboks.xyz>
2021-02-19 12:35:34 +0100

    tuning

--- include/default_params.h
+++ include/default_params.h
@@ -22,12 +22,16 @@ double params::WS_alpha = 0.4;
 // Probability
 double params::AP_beta = 0.6;
 double params::PM_AP_pMin_divider = 3;
-std::function<double(int)> const params::PM_AP_pMin = [](int const K) -> double {return 1. / (params::PM_AP_pMin_divider * (K-1.));};
-std::function<double(int)> const params::AP_pMax = [](int const K) -> double {return 1. - (K-1.) * params::PM_AP_pMin(K);};
+std::function<double(int)> const params::PM_AP_pMin =
+  [](int const K) -> double {return 1. / (params::PM_AP_pMin_divider * (K-1.));};
+std::function<double(int)> const params::AP_pMax = 
+  [](int const K) -> double {return 1. - (K-1.) * params::PM_AP_pMin(K);};
 
 // Parameter self-adaptation
-std::function<int(int, int)> const params::SHADE_H = [](int const popSize, int const K) -> int {return std::max(popSize/K, 10);};
-std::function<int(int)> const params::SHADE_p = [](int const popSize) -> int {return rng.randInt(0, rng.randInt(2, popSize/5)-1);};
+std::function<int(int, int)> const params::SHADE_H = 
+  [](int const popSize, int const K) -> int {return std::max(popSize/K, 10);};
+std::function<int(int)> const params::SHADE_p = 
+  [](int const popSize) -> int {return rng.randInt(0, rng.randInt(2, popSize/5)-1);};
 
 // Constraint handling
 int const params::RS_max_resamples = 100;
