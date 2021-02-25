2cc44ffe30b4b22f9b8a43982512012c0b582a4d
Rick Boks <rickboks@hotmail.com>
2021-02-25 14:55:13 +0100

    prepare tuning experiment 2

--- irace/generate_runners.fish
+++ irace/generate_runners.fish
@@ -1,8 +1,7 @@
 #!/usr/bin/env fish
 for c in (cat best_configs.dat)
 	set credit (string split -- " " $c)[2]
-	awk -v conf="$c" -v cred="$credit" \
-		'/^FIXED_PARAMS=/{sub(/__FIXED_PARAMS__/,conf)};
-		/^CREDIT=|^LOG_DIR=/{sub(/__CREDIT__/,cred)} {print}' \
+	awk -v cred="$credit" \
+		'/^CREDIT=/{sub(/__CREDIT__/,cred)} {print}' \
 		target-runner-template > runners/$credit.runner
 end
