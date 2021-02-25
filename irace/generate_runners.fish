67f1d905ce248959cd786a4bd67858ca2d97e6cb
Rick Boks <rick@rickboks.xyz>
2021-02-25 15:04:49 +0100

    preparing experiment

--- irace/generate_runners.fish
+++ irace/generate_runners.fish
@@ -1,7 +1,6 @@
 #!/usr/bin/env fish
-for c in (cat best_configs.dat)
-	set credit (string split -- " " $c)[2]
-	awk -v cred="$credit" \
+for c in "FI" "FD" "FS" "DR" "SD" "PA" "CO"
+	awk -v cred="$c" \
 		'/^CREDIT=/{sub(/__CREDIT__/,cred)} {print}' \
-		target-runner-template > runners/$credit.runner
+		target-runner-template > runners/$c.runner
 end
--- irace/tune.fish
+++ irace/tune.fish
@@ -5,26 +5,18 @@ if test (count $argv) -lt 1
 end
 
 set d "$HOME/auto-DE/irace"
-
 set cred $argv[1]
-set prob (cat best_configs.dat | awk -v c=$cred '$2 == c {print $6}') 
-
-if test ! (count $prob) = 1
-	echo "Invalid credit manager"
-	exit
-end
 
 mkdir -p results/$cred logs/$cred
 
-irace --parameter-file "$d/parameters-$prob.txt" \
+irace --parameter-file "$d/parameters.txt" \
 	--train-instances-file "$d/instances-list.txt" \
-	--configurations-file "$d/configurations-$prob.txt" \
-	--forbidden-file "$d/forbidden-$prob.txt" \
+	--forbidden-file "$d/forbidden.txt" \
 	--target-runner "$d/runners/$cred.runner" \
-	--max-experiments "100000" \
+	--max-experiments "5000" \
 	--parallel (nproc --all) \
 	--digits 2 \
 	-l results/$cred/result.Rdata \
 	-s $d/scenario.txt \
 	--exec-dir $HOME/auto-DE/irace
-
+	#--configurations-file "$d/configurations.txt" \
