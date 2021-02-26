0b0710550060c67b8cc40d7912c6a51388ca5056
Rick Boks <rick@rickboks.xyz>
2021-02-26 12:09:58 +0100

    use seconds instead of millis

--- irace/tune.fish
+++ irace/tune.fish
@@ -14,9 +14,10 @@ irace --parameter-file "$d/parameters.txt" \
 	--train-instances-file "$d/instances-list.txt" \
 	--forbidden-file "$d/forbidden.txt" \
 	--target-runner "$d/runners/$cred.runner" \
-	--max-time (math "$HOURS * 3600") \
+	--max-time (math "15 * 60") \
 	--parallel (nproc --all) \
 	--digits 2 \
 	-l results/$cred/result.Rdata \
 	-s $d/scenario.txt \
 	--exec-dir $HOME/auto-DE/irace
+	#--max-time (math "$HOURS * 3600") \
