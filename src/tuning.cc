5e125027414f908626c440f0b4c062039c3cbbac
Rick Boks <rickboks@hotmail.com>
2021-02-26 11:21:55 +0100

    remove comment

--- src/tuning.cc
+++ src/tuning.cc
@@ -8,7 +8,7 @@
 static int const BUDGET_MULTIPLIER = 1e4;
 
 #include "default_params.h"
-//double coco_problem_get_final_target_fvalue1(const coco_problem_t *problem);
+
 void experiment(DifferentialEvolution& de,
 				char const *const suite_name,
 				char const *const suite_options,
