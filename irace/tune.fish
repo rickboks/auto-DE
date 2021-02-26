23f2937ca158f2f17ccf684d7b84b0483f9516e1
Rick Boks <rick@rickboks.xyz>
2021-02-26 12:57:19 +0100

    tabs to spaces

--- irace/tune.fish
+++ irace/tune.fish
@@ -6,18 +6,17 @@ end
 
 set d "$HOME/auto-DE/irace"
 set cred $argv[1]
-set HOURS 48
+set HOURS 60
 
-mkdir -p results/$cred logs/$cred
+mkdir -p results logs/$cred recovery
 
 irace --parameter-file "$d/parameters.txt" \
 	--train-instances-file "$d/instances-list.txt" \
 	--forbidden-file "$d/forbidden.txt" \
 	--target-runner "$d/runners/$cred.runner" \
-	--max-time (math "15 * 60") \
+	--max-time (math "$HOURS * 3600") \
 	--parallel (nproc --all) \
 	--digits 2 \
-	-l results/$cred/result.Rdata \
+	-l results/$cred.Rdata \
 	-s $d/scenario.txt \
 	--exec-dir $HOME/auto-DE/irace
-	#--max-time (math "$HOURS * 3600") \
