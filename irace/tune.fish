8a172b30f49448301d3ceb9ebb017850ac60f492
Rick Boks <rick@rickboks.xyz>
2021-02-26 18:18:20 +0100

    tuning running with budget 1e5 - now really

--- irace/tune.fish
+++ irace/tune.fish
@@ -6,7 +6,7 @@ end
 
 set d "$HOME/auto-DE/irace"
 set cred $argv[1]
-set HOURS 62
+set BUDGET 100000
 set threads (nproc --all)
 
 mkdir -p results logs/$cred
@@ -16,10 +16,10 @@ irace --parameter-file "$d/parameters.txt" \
 	--forbidden-file "$d/forbidden.txt" \
 	--target-runner "$d/runners/$cred.runner" \
 	--seed 44 \
-	--max-time (math "$threads * $HOURS * 3600") \
+	--max-experiments $BUDGET \
 	--parallel $threads \
 	--digits 2 \
 	-l results/$cred.Rdata \
 	-s $d/scenario.txt \
 	--exec-dir $HOME/auto-DE/irace \
-	--budget-estimation (math "0.02 * (6 / $threads)") \
+	--elitist 0 \
