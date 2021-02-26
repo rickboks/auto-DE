840c5fd34338bb5bd962a762445f0be96ab26c4a
Rick Boks <rick@rickboks.xyz>
2021-02-26 12:01:48 +0100

    use seconds instead of millis

--- irace/tune.fish
+++ irace/tune.fish
@@ -6,6 +6,7 @@ end
 
 set d "$HOME/auto-DE/irace"
 set cred $argv[1]
+set HOURS 48
 
 mkdir -p results/$cred logs/$cred
 
@@ -13,10 +14,9 @@ irace --parameter-file "$d/parameters.txt" \
 	--train-instances-file "$d/instances-list.txt" \
 	--forbidden-file "$d/forbidden.txt" \
 	--target-runner "$d/runners/$cred.runner" \
-	--max-experiments "500000" \
+	--max-time (math "$HOURS * 3600") \
 	--parallel (nproc --all) \
 	--digits 2 \
 	-l results/$cred/result.Rdata \
 	-s $d/scenario.txt \
 	--exec-dir $HOME/auto-DE/irace
-	#--configurations-file "$d/configurations.txt" \
