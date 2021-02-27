28fb2c005adc628ef214558159e1370ec100449d
Rick Boks <rick@rickboks.xyz>
2021-02-27 14:54:30 +0100

    rerunning with elitist

--- irace/tune.fish
+++ irace/tune.fish
@@ -9,17 +9,16 @@ set cred $argv[1]
 set BUDGET 100000
 set threads (nproc --all)
 
-mkdir -p results logs/$cred
+mkdir -p results logs/$cred output
 
 irace --parameter-file "$d/parameters.txt" \
 	--train-instances-file "$d/instances-list.txt" \
 	--forbidden-file "$d/forbidden.txt" \
 	--target-runner "$d/runners/$cred.runner" \
-	--seed 44 \
+	--seed 42 \
 	--max-experiments $BUDGET \
 	--parallel $threads \
 	--digits 2 \
 	-l results/$cred.Rdata \
 	-s $d/scenario.txt \
-	--exec-dir $HOME/auto-DE/irace \
-	--elitist 0
+	--exec-dir $HOME/auto-DE/irace > output/$cred.log
