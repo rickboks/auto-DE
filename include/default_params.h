7a9438120208a87ac994a9512a11c5bf3e9f3fed
Rick Boks <rick@rickboks.xyz>
2021-03-12 09:50:18 +0100

    params changed

--- include/default_params.h
+++ include/default_params.h
@@ -2,9 +2,9 @@
 bool params::log_activations = true;
 int const params::log_activations_interval = 1;
 bool params::log_parameters = true;
-int const params::log_parameters_interval = 5;
+int const params::log_parameters_interval = 1;
 bool params::log_diversity = true;
-int const params::log_diversity_interval = 5;
+int const params::log_diversity_interval = 1;
 bool params::log_positions = false;
 int const params::log_positions_interval = 1;
 std::string const params::extra_data_path = "./extra_data";
