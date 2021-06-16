bb47dddfa340890510835c9a31cb1964ca897c46
Rick Boks <rick@rickboks.com>
2021-06-16 23:06:27 +0200

    removed some obsolete functions

--- src/util.cc
+++ src/util.cc
@@ -1,26 +1,5 @@
 #include "util.h"
 #include "rng.h"
-#include <experimental/filesystem>
-#include <fstream>
-
-std::string generateConfig(std::string const templateFile, std::string const name){
-	std::string const folder = "configurations";
-	std::string const cfgFile = folder + "/" + name + ".ini";
-
-	if (!std::experimental::filesystem::exists(folder)){
-		std::cerr << "Creating directory \"" << folder << "\"."<< std::endl;
-		std::experimental::filesystem::create_directory(folder);
-	}
-
-	std::ifstream src(templateFile, std::ios::binary);
-	std::ofstream dst(cfgFile, std::ios::binary);
-
-	dst << src.rdbuf() 
-		<< "result_folder = " + name << std::endl 
-		<< "algorithm_name = " + name << std::endl;
-
-	return cfgFile;
-}
 
 double distance(Solution const*const s1, Solution const*const s2) {
 	return distance(s1->X(), s2->X());
@@ -28,16 +7,6 @@ double distance(Solution const*const s1, Solution const*const s2) {
 
 double distance(ArrayXd const& s1, ArrayXd const& s2){
 	return (s1 - s2).matrix().norm();
-}
-
-std::string checkFilename(std::string const fn){
-	std::string newFn = fn;
-	int i = 1;
-	while (std::experimental::filesystem::exists(newFn)){
-		newFn = fn + "." + std::to_string(i);
-		i++;
-	}
-	return newFn;
 }
 
 std::vector<int> range(int const size){
